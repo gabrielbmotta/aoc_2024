115af65d917b9363760794baa19c3367707e12a7
Gabriel B. Motta <gabrielbenmotta@gmail.com>
2024-12-11 21:45:52 -0500

    day 11 part 2, do unique operations only

--- day_11/gbm_py/day11.py
+++ day_11/gbm_py/day11.py
@@ -1,20 +1,30 @@
+from collections import Counter
+
+
 def load_data(path):
     file = open(path, mode='r')
     return [int(n) for n in file.readlines()[0].split()]
 
 
 def blink(input):
-    output = list()
+    output = dict()
 
-    for stone in input:
+    def add_to_dict(v, n):
+        nonlocal output
+        if v in output:
+            output[v] += n
+        else:
+            output[v] = n
+
+    for stone, num in input.items():
         if stone == 0:
-            output.append(1)
+            add_to_dict(1, num)
         elif len(str(stone)) % 2 == 0:
             mid = len(str(stone)) // 2
-            output.append(int(str(stone)[0:mid]))
-            output.append(int(str(stone)[mid:]))
+            add_to_dict(int(str(stone)[0:mid]), num)
+            add_to_dict(int(str(stone)[mid:]), num)
         else:
-            output.append(stone * 2024)
+            add_to_dict(stone * 2024, num)
 
     return output
 
@@ -22,13 +32,12 @@ def blink(input):
 def blink_repeatedly(data, num_blinks):
     for i in range(num_blinks):
         data = blink(data)
-        print(i)
     return data
 
 
 if __name__ == "__main__":
-    # for file in ["test.txt", "input.txt"]:
-    for file in ["test.txt"]:
+    for file in ["test.txt", "input.txt"]:
+    # for file in ["test.txt"]:
         print(f"== Parsing {file} ==")
         try:
             data = load_data(file)
@@ -36,5 +45,14 @@ if __name__ == "__main__":
             print("[!] File not found, skipping.")
             continue
 
-        print(f"Part 1: {len(blink_repeatedly(data, 25))}")
-        # print(f"Part 2: {len(blink_repeatedly(data, 75))}")
+        blink25 = blink_repeatedly(dict(Counter(data)), 25)
+        p1_sum = 0
+        for _, n in blink25.items():
+            p1_sum += n
+        print(f"Part 1: {p1_sum}")
+
+        blink75 = blink_repeatedly(dict(Counter(data)), 75)
+        p2_sum = 0
+        for _, n in blink75.items():
+            p2_sum += n
+        print(f"Part 2: {p2_sum}")
